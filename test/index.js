c4721b10948cd36073a95b55f12f712ffc21a7c4
Roman <stioneq@gmail.com>
2019-06-15 22:31:41 +0300

    1.0.8 add tests

--- test/index.js
+++ test/index.js
@@ -2,7 +2,8 @@
 
 const expect = require('chai').expect;
 const should = require('chai').should();
-const {crudify} = require('../dist/index.js');
+const {crudify, plugins} = require('../dist/index.js');
+const {createdAt, modifiedAt} = plugins;
 const mongo = require('mongodb');
 const TEST_DB_URL = 'mongodb://localhost:27017';
 
@@ -84,20 +85,29 @@ describe('Mongo Crudify', function () {
         });
         it('should correctly findOne item', async () => {
             const _id = new mongo.ObjectId().toString();
-            await client.db('test').collection('testCollection').insertMany([{_id: new mongo.ObjectId(_id), a: 1}, {b: 2}]);
+            await client.db('test').collection('testCollection').insertMany([{
+                _id: new mongo.ObjectId(_id),
+                a: 1
+            }, {b: 2}]);
             const result = await Crudify.findOne(_id);
             expect(result).to.own.include({a: 1});
         });
         it('should correctly deleteOne item', async () => {
             const _id = new mongo.ObjectId().toString();
-            await client.db('test').collection('testCollection').insertMany([{_id: new mongo.ObjectId(_id), a: 1}, {b: 2}]);
+            await client.db('test').collection('testCollection').insertMany([{
+                _id: new mongo.ObjectId(_id),
+                a: 1
+            }, {b: 2}]);
             const result = await Crudify.deleteOne(_id);
             const items = await client.db('test').collection('testCollection').find().toArray();
             expect(items).to.have.length(1);
         });
         it('should correctly updateOne item', async () => {
             const _id = new mongo.ObjectId().toString();
-            await client.db('test').collection('testCollection').insertMany([{_id: new mongo.ObjectId(_id), a: 1}, {b: 2}]);
+            await client.db('test').collection('testCollection').insertMany([{
+                _id: new mongo.ObjectId(_id),
+                a: 1
+            }, {b: 2}]);
             const result = await Crudify.updateOne({_id, a: 512});
             const items = await client.db('test').collection('testCollection').find().toArray();
             expect(items[0]).to.own.include({a: 512});
@@ -105,12 +115,63 @@ describe('Mongo Crudify', function () {
         });
         it('should correctly insert item', async () => {
             const _id = new mongo.ObjectId().toString();
-            await client.db('test').collection('testCollection').insertMany([{_id: new mongo.ObjectId(_id), a: 1}, {b: 2}]);
+            await client.db('test').collection('testCollection').insertMany([{
+                _id: new mongo.ObjectId(_id),
+                a: 1
+            }, {b: 2}]);
             await Crudify.insertOne({b: 'hello'});
             const items = await client.db('test').collection('testCollection').find().toArray();
             expect(items).to.have.length(3);
-            expect(items.some(item => item.b ==='hello')).to.be.true;
+            expect(items.some(item => item.b === 'hello')).to.be.true;
         });
     });
+    describe('Plugins integration tests', () => {
+        let Crudify;
+        let client;
+        before(async () => {
+            client = await mongo.connect('mongodb://localhost:27017', {
+                useNewUrlParser: true
+            });
 
+            Crudify = crudify(client, 'test', 'testCollection');
+        });
+        after(() => {
+            client.close();
+        });
+
+        beforeEach(async () => {
+            client.db('test').collection('testCollection').deleteMany({});
+        });
+        it('should add new date field to the inserted items', async () => {
+
+            Crudify.use(createdAt());
+
+            await Crudify.insertOne({a: 1});
+            const result = await client.db('test').collection('testCollection').find({}).toArray();
+            expect(result).to.has.length(1);
+            expect(result[0].createdAt).to.be.a('date');
+        });
+        it('should add modifiedAt date field to the updated items', async () => {
+
+            Crudify.use(modifiedAt());
+            const _id = new mongo.ObjectId().toString();
+            await client.db('test').collection('testCollection').insertMany([{
+                _id: new mongo.ObjectId(_id),
+                a: 1
+            }, {b: 2}]);
+            const result = await Crudify.updateOne({_id, a: 512});
+            const items = await client.db('test').collection('testCollection').find().toArray();
+            expect(items[0].modifiedAt).to.be.a('date');
+        });
+        it('should throw exception when use middleware on the unexisted operation', async () => {
+
+            expect(() => Crudify.use('myOperation', async (data) => data)).to.throw('');
+
+        });
+        it('should throw exception if there is no args passed to use function', async () => {
+
+            expect(() => Crudify.use()).to.throw('');
+
+        });
+    });
 });
